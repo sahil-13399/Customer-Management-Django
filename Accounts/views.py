6bfc0992442b9b8c148ebd11423ecd132972cf47
sahil <sahil13399@gmail.com>
2020-07-23 20:22:44 +0530

    Updated Filter Search *No user login Module*

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 # Create your views here.
 from .models import *
 from .forms import OrderForm
+from .filter import OrderFilter
+
+
 
 def home(request):
 	orders = Order.objects.all()
@@ -31,21 +35,28 @@ def customer(request, pk_test):
 	orders = customer.order_set.all()
 	order_count = orders.count()
 
-	context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+	myFilter = OrderFilter(request.GET, queryset=orders)
+	orders = myFilter.qs 
+
+	context = {'customer':customer, 'orders':orders, 'order_count':order_count,
+	'myFilter':myFilter}
 	return render(request, 'Accounts/customer.html',context)
 
-def createOrder(request):
-	form = OrderForm()
+def createOrder(request, pk):
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+	customer = Customer.objects.get(id=pk)
+	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+	#form = OrderForm(initial={'customer':customer})
 	if request.method == 'POST':
 		#print('Printing POST:', request.POST)
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
+		#form = OrderForm(request.POST)
+		formset = OrderFormSet(request.POST, instance=customer)
+		if formset.is_valid():
+			formset.save()
 			return redirect('/')
 
-	context = {'form':form}
+	context = {'form':formset}
 	return render(request, 'Accounts/order_form.html', context)
-
 def updateOrder(request, pk):
 
 	order = Order.objects.get(id=pk)
