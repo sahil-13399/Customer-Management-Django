f6ba198a03cecbaed126eeb8be852174f6dead5e
sahil <sahil13399@gmail.com>
2020-07-23 17:44:18 +0530

    CRUD functionality added

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from . import views 
+from django.urls import path
+from . import views
+
+
 urlpatterns = [
-   path('',views.home),
-   path('products/',views.products),
-   path('customer/',views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+
+    path('create_order/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
 ]
