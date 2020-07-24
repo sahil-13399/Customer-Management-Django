57a00eadaa3ce567c6204c3860394b8ee4da91c0
sahil <sahil13399@gmail.com>
2020-07-24 22:47:13 +0530

    alldone

--- Customer_Management_Portal/settings.py
+++ Customer_Management_Portal/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+      'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
